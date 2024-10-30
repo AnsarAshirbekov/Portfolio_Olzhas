8eb82d34a7d93efd1701acc0d2240b813cf14e5c
Your Name <you@example.com>
2024-10-30 21:12:10 +0500

    models agregate

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 
+class Publisher(models.Model):
+    name = models.CharField(max_length=100)
+    city = models.CharField(max_length=30)
+    is_gover = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.name} ({self.city})'
+
+
 class Book(models.Model):
     title = models.CharField(max_length=100, unique=True)
     published_date = models.DateField()
@@ -8,3 +17,10 @@ class Book(models.Model):
     author = models.CharField(max_length=100, blank=True)
     price = models.DecimalField(max_digits = 10, decimal_places=2, default=0)
     rating = models.DecimalField(max_digits = 10, decimal_places=2, null=True)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE, related_name='publisher', null=True)
+
+    def __str__(self):
+        return f'{self.title} ({self.published_date.year})'
+
+
+    
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Book
+from django.db.models import Avg, Count, Min, Max, Sum
 
 def about_me(request):
     return render(request, 'about_me.html')
@@ -11,8 +12,36 @@ def portfolio(request):
     return render(request, 'portfolio.html')
 
 def books(request):
-    books = Book.objects.all()
+    books_all = Book.objects.all()
+    books_first = Book.objects.first()
+    books_last = Book.objects.last()
+    books_get = Book.objects.get(title = 'Хроники Нарний')
+    books_filter_publisher = Book.objects.filter(publisher__name = 'Бразука')
+    books_date = Book.objects.filter(published_date__year__gte = 1995)
+    books_agreg = Book.objects.aggregate(Avg('rating'))
+
+    max_pages = Book.objects.aggregate(Max('pages'))
+    book_max_pages = Book.objects.filter(pages = max_pages['pages__max'])
+    
+    book_oldest = Book.objects.aggregate(Min('published_date'))
+    book_oldest_name = Book.objects.filter(published_date = book_oldest['published_date__min'])
+
+    pages_quantity = Book.objects.aggregate(Sum('pages'))
+
+    books_quantity = Book.objects.aggregate(Count('title'))
+
     context = {
-        "books" : books
+        "books_all" : books_all,
+        "books_first" : books_first,
+        "books_last" : books_last,
+        "books_get" : books_get,
+        "books_filter_publisher" : books_filter_publisher,
+        "books_date" : books_date,
+        "books_agreg" : books_agreg,
+        "book_max_pages" : book_max_pages,
+        "book_oldest_name" : book_oldest_name,
+        "pages_quantity" : pages_quantity,
+        "books_quantity" : books_quantity,
     }
     return render(request, 'books.html', context)
+    
